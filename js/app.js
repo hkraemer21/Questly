a35a211f00886130962b04476b0d64d7cbf39c28
hkraemer21 <hkraemer1@my.wctc.edu>
2026-03-02 23:45:38 -0600

    Updated more of the UI, including the game name list and the achievements section. Added increment and decrement for achievements.

--- js/app.js
+++ js/app.js
@@ -23,31 +23,32 @@ const app = createApp({
                 description: '',
                 increment: 0,
                 pin: false,
-                favorite: false,
+                complete: false,
             },
 
             gameList: [
                 {
                     name: 'Resident Evil 2 Remake', image: 're2remake.jpeg', platform: 'PC', pin: true, favorite: true, complete: true, achievements: [
-                        { name: 'Welcome to Raccoon City', description: 'Complete the game on any difficulty.', increment: 1, pin: false, favorite: false },
-                        { name: 'Survivor', description: 'Complete the game on Normal or higher difficulty.', increment: 1, pin: false, favorite: false },
-                        { name: 'Master of Unlocking', description: 'Unlock all the weapons and items in the game.', increment: 1, pin: false, favorite: false },
+                        { name: 'Welcome to Raccoon City', description: 'Complete the game on any difficulty.', increment: 1, pin: false, complete: true },
+                        { name: 'Survivor', description: 'Complete the game on Normal or higher difficulty.', increment: 1, pin: false, complete: false },
+                        { name: 'Master of Unlocking', description: 'Unlock all the weapons and items in the game.', increment: 1, pin: false, complete: false },
                     ]
                 },
                 {
                     name: 'God of War: Ragnarok', image: 'gowragnarok.jpg', platform: 'PS5', pin: false, favorite: true, complete: false, achievements: [
-                        { name: 'Ragnarok Unleashed', description: 'Complete the game on any difficulty.', increment: 1, pin: false, favorite: false },
-                        { name: 'Valhalla Conqueror', description: 'Complete the game on Hard difficulty.', increment: 1, pin: false, favorite: false },
-                        { name: 'Mythical Collector', description: 'Collect all the collectibles in the game.', increment: 1, pin: false, favorite: false },
+                        { name: 'Ragnarok Unleashed', description: 'Complete the game on any difficulty.', increment: 1, pin: false, complete: false },
+                        { name: 'Valhalla Conqueror', description: 'Complete the game on Hard difficulty.', increment: 1, pin: false, complete: false },
+                        { name: 'Mythical Collector', description: 'Collect all the collectibles in the game.', increment: 1, pin: false, complete: false },
                     ]
                 },
                 {
                     name: 'The Last of Us Part II', image: 'tloup2.jpg', platform: 'PS5', pin: false, favorite: false, complete: false, achievements: [
-                        { name: 'Survivor', description: 'Complete the game on any difficulty.', increment: 1, pin: false, favorite: false },
-                        { name: 'Master of Stealth', description: 'Complete the game using only stealth tactics.', increment: 1, pin: false, favorite: false },
-                        { name: 'Collector', description: 'Collect all the collectibles in the game.', increment: 1, pin: false, favorite: false },
+                        { name: 'Survivor', description: 'Complete the game on any difficulty.', increment: 1, pin: false, complete: false },
+                        { name: 'Master of Stealth', description: 'Complete the game using only stealth tactics.', increment: 1, pin: false, complete: false },
+                        { name: 'Collector', description: 'Collect all the collectibles in the game.', increment: 1, pin: false, complete: false },
                     ]
                 },
+                
 
             ],
 
@@ -97,6 +98,27 @@ const app = createApp({
 
     // computed: values that are updated and cached if dependencies change
     computed: {
+        favoritedGames: function () {
+            return this.gameList.filter(game => game.favorite);
+        },
+
+        pinnedGames: function () {
+            return this.gameList.filter(game => game.pin);
+        },
+
+        favoritedAchievements: function () {
+            if (this.selectedGame) {
+                return this.selectedGame.achievements.filter(achievement => achievement.favorite);
+            }
+            return [];
+        },
+
+        pinnedAchievements: function () {
+            if (this.selectedGame) {
+                return this.selectedGame.achievements.filter(achievement => achievement.pin);
+            }
+            return [];
+        },
 
     },
 
